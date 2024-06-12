3702f4488b2fbdd6f32ee31924a4cab44cb6b241
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2024-06-12 12:03:30 +0200

    Add project files.

---
+++ SnakeHrdy/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SnakeHrdy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int SnakeSquareSize = 20;
+        private const int InitialSnakeLenght = 5;
+        private const int SnakeStartX = 100;
+        private const int SnakeStartY = 100;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
